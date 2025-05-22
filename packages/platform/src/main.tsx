0e10140d65b338e844cbd8d4d4969468b8aae588
Zioncesss <zioncesss@gmail.com>
2025-05-22 22:54:02 +0800

    chore:初始化platforme结构

--- packages/platform/src/main.tsx
+++ packages/platform/src/main.tsx
@@ -2,17 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import WebSDK from "@monitor-platform/sdk";
+import { BrowserRouter } from "react-router-dom";
+/*先注释掉sdk引入 */
+// import WebSDK from "@monitor-platform/sdk";
 
-const config = {
-  project_id: "1234567890",
-};
+// const config = {
+//   project_id: "1234567890",
+// };
 
-const webSDK = new WebSDK(config);
-console.log(webSDK);
+// const webSDK = new WebSDK(config);
+// console.log(webSDK);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
