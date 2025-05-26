c8f7c1ecfe32a4398dbaf293386643d5da73e3c3
hanser-li <2641210631@qq.com>
2025-05-26 15:23:09 +0800

    feat: 增加用户行为监控入口

--- packages/webSDK/src/plugins/behavior/index.ts
+++ packages/webSDK/src/plugins/behavior/index.ts
@@ -5,19 +5,22 @@ import { PageLifeCycleMonitor } from "./PageLifeCycleMonitor";
 import WebSDK from "../..";
 
 export class BehaviorMonitor {
-  public clickMonitor: ClickMonitor;
-  public scrollMonitor: ScrollMonitor;
-  public pageLifeCycleMonitor: PageLifeCycleMonitor;
+  public clickMonitor: ClickMonitor| undefined;
+  public scrollMonitor: ScrollMonitor| undefined;
+  public pageLifeCycleMonitor: PageLifeCycleMonitor| undefined;
   private sdkInstance: WebSDK;
 
   constructor(sdkInstance:WebSDK ) {
   this.sdkInstance = sdkInstance;
   // 初始化所有监控器
-  this.clickMonitor = new ClickMonitor(sdkInstance);
-  this.scrollMonitor = new ScrollMonitor(sdkInstance);
-  this.pageLifeCycleMonitor = new PageLifeCycleMonitor(sdkInstance);
+  this.initAllMonitors();
   }
-  
+  private initAllMonitors(): void {
+    this. pageLifeCycleMonitor = new PageLifeCycleMonitor(this.sdkInstance);   
+     // 页面生命周期指标
+    this.clickMonitor = new ClickMonitor(this.sdkInstance);
+    this.scrollMonitor = new ScrollMonitor(this.sdkInstance);
+  }
   // 可选：提供统一销毁方法
     public destroy() {
       // 假设后续会在各监控器类中添加 destroy 方法，此处先做类型断言检查
