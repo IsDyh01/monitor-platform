1f56ccccca4c7fddc53de558ad293ceb86df32d2
Zioncesss <zioncesss@gmail.com>
2025-05-29 18:23:59 +0800

    feat:平台框架目录更新，埋点管理页面完成新增埋点到后端，从数据库引入数据渲染到表格

---
+++ packages/platform/src/pages/ErrorMonitor/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorMonitor: React.FC = () => {
+  return (
+    <div>
+      错误监控页面
+    </div>
+  )
+} 
+
+export default ErrorMonitor
---
+++ packages/platform/src/pages/Performance/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Performance: React.FC = () => {
+  return (
+    <div>
+      行为监控
+    </div>
+  )
+} 
+
+export default Performance
